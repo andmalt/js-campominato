b53562f3a9af852a5fa4991f96fa57517a21dcd5
andmalt <andrea.maltempi@gmail.com>
2021-09-02 19:22:57 +0200

    inserito livello di difficoltà

--- Js/myscript.js
+++ Js/myscript.js
@@ -12,9 +12,33 @@ let arrayNumeri = [];
 // i numeri del pc
 let pcNumber = [];
 
+// variabile numero bombe
+let numberBomb;
+
+// vita del giocatore
+let life;
+
+// numeri dell'utente da raggiungere per vittoria
+let numberWin;
+
+// chiedo il livello di difficoltà
+let arrayLevel = ["facile","medio","difficile"];
+
+let difficultyLevel = prompt("Inserisci livello di difficoltà");
+while ((difficultyLevel.length === 0) || ( !arrayLevel.includes(difficultyLevel.trim().toLowerCase()))){
+    difficultyLevel = prompt("Reinserisci livello di difficoltà");
+}
+
+let variabileProvvisoria = getGameLevel(difficultyLevel);
+
+life = variabileProvvisoria[0];
+numberBomb = variabileProvvisoria[1];
+numberWin = variabileProvvisoria[2];
+
+
 // creo 16 numeri casuali diversi da inserire nell'array del pc
 let i = 0;
-while(pcNumber.length < 16){
+while(pcNumber.length < numberBomb){
     arrayNumeri.push(getRandomInt(1 , 100));
     if(pcNumber.indexOf(arrayNumeri[i]) === -1){
         pcNumber.push(arrayNumeri[i]);
@@ -26,16 +50,14 @@ console.log("Numeri del pc: "+pcNumber);
 // creo array utente
 let numeriUtente = [];
 
-// vita del giocatore
-let status = 1;
 
 // chiedo all'utente dei numeri da inserire controllando che non si ripetano e non siano uguali a quelli del pc
-while((numeriUtente.length < 6) && (status > 0)){
+while((numeriUtente.length < numberWin) && (life > 0)){
     let numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 100"));
-    if(numeroUtente>0 && numeroUtente<101){
+    if((numeroUtente>0) && (numeroUtente<101)){
         if(pcNumber.includes(numeroUtente) === true){
             alert("KABOOOOOOOOOMMM");
-            status = status - 1;
+            life = life - 1;
         }else if(numeriUtente.indexOf(numeroUtente) === -1){
             numeriUtente.push(numeroUtente);
         }else{
@@ -47,11 +69,49 @@ while((numeriUtente.length < 6) && (status > 0)){
     i++;
 }
 
-if(status === 0){
+if(life === 0){
     alert("Hai perso!!! ritenta");
-}else if(numeriUtente.length == 6){
+}else if(numeriUtente.length == numberWin){
     alert("Complimenti hai vinto!!");
 }
+
+
+// funzione per il livello
+function getGameLevel(gameLevel){
+
+    let vita;
+    let numeroBombe;
+    let numeroPunti;
+
+    switch (gameLevel){
+        
+        case "facile":
+            vita = 3;
+            numeroBombe = 16;
+            numeroPunti = 6;
+            break;
+
+            case "medio":
+                vita = 2;
+                numeroBombe = 16;
+                numeroPunti = 12;
+                break;
+
+                case "difficile":
+                    vita = 1;
+                    numeroBombe = 25;
+                    numeroPunti = 25;
+                    break;
+
+                    default:
+                        vita = 3;
+                        numeroBombe = 16;
+                        numeroPunti = 6;
+    }
+
+    return[vita , numeroBombe , numeroPunti];
+}
+
 
 console.log("Punteggio: "+numeriUtente.length);
 
